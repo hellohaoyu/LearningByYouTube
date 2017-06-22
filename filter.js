7391993359b6824962257c7dca5be76d1844c9de
Haoyu-Mac <hc6as@virginia.edu>
2017-06-21 19:45:30 -0700

    Add comments and clean code

--- filter.js
+++ filter.js
@@ -1,26 +1,29 @@
+//Introduct two high-order functions of list in JS.
+
 var animals = 
   [ {name: "Amy", species: "dog"},
     {name: "Kat", species: "rabbit"},
     {name: "Sid", species: "cat"},
     {name: "Patty", species: "dog"}, ];
 
+// High-Order function -> Filter
+var dogs = animals.filter(function (animal) {
+	return animal.species == "dog";
+});
 
-// var dogs = animals.filter(function (animal) {
-// 	return animal.species == "dog";
-// });
+var isDog = function (animal) {
+	return animal.species === "dog";
+};
 
-// var isDog = function (animal) {
-// 	return animal.species === "dog";
-// };
+var dogs = animals.filter(isDog);
+console.log(dogs);
 
-// var dogs = animals.filter(isDog);
-
+// High-Order function -> Map
 var animalNames = animals.map(function(animal) {
 	return animal.name;
 });
 
+// Error -> arrow function is supported until ES6.
+// var animalNames = animals.map((animal) => animal.name);
 
-
-// console.log(dogs);
-// console.log(nonDogs);
 console.log(animalNames);
